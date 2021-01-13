4e2490dccb216958ba94e22c87b7a6134e7bc74b
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-13 15:56:45 +0900

    oup modif

--- Test_no_reload.py
+++ Test_no_reload.py
@@ -47,6 +47,10 @@ hp.lr_start = 1e-6
 hp.lr_end = 1e-7
 hp.lr_nsteps = 100000
 
+hp.OUP_stddev_start = 2.0
+hp.OUP_stddev_end = 0.2
+hp.OUP_stddev_nstep = 100000
+
 
 if args.render :
     from gym.envs.classic_control.rendering import SimpleImageViewer
