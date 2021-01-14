9c27f08d836c23cf8af463a3362ff465a78e17c3
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-14 17:03:32 +0900

    lr/oup modif

--- Test_no_reload.py
+++ Test_no_reload.py
@@ -43,11 +43,11 @@ my_tqdm = tqdm(total=total_steps, dynamic_ncols=True)
 hp.Model_save = 30000
 hp.Learn_start = 20000
 
-hp.lr_start = 1e-5
-hp.lr_end = 1e-10
+hp.lr_start = 1e-6
+hp.lr_end = 1e-6
 hp.lr_nsteps = 1000000
 
-hp.OUP_stddev_start = 1.0
+hp.OUP_stddev_start = 0.2
 hp.OUP_stddev_end = 0.2
 hp.OUP_stddev_nstep = 200000
 
