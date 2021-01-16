f30c060ffc8406dfd0fb107d9d21a05db10c7f70
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-17 01:17:20 +0900

    longer test

--- env_test.py
+++ env_test.py
@@ -21,8 +21,8 @@ env.reset()
 # diff = 0
 # for _ in trange(100):
     # diff += env.check_step(env.action_space.sample())
-for _ in trange(1000):
-    o, r, d, i = env.step(np.array([0.5,1]))
+for _ in trange(3000):
+    o, r, d, i = env.step(np.array([0.3,0.3]))
     writer.write(o['obs'][...,-1:-4:-1])
     writer_ren.write(env.render('rgb')[...,::-1])
     if d :
