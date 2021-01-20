4ce162afac910c2c62e597a46b463bc2718e1bb4
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-20 12:40:45 +0900

    making iqn model

--- agent_assets/critic_models.py
+++ agent_assets/critic_models.py
@@ -42,3 +42,34 @@ def critic_simple_dense(observation_space, action_space, encoder_f):
     )
 
     return model
+
+IQN_SUPPORT = 64
+
+def critic_dense_iqn(observation_space, action_space, encoder_f):
+    action_input = keras.Input(action_space.shape,
+                            name='action')
+    encoded_state, encoder_inputs = encoder_f(observation_space)
+    s = layers.Flatten(name='critic_flatten_state')(encoded_state)
+    a = layers.Flatten(name='critic_flatten_action')(action_input)
+
+    x = layers.Concatenate(name='critic_concat_action_state')([s,a])
+
+    x = layers.Dense(256, activation='relu',
+                     name='critic_dense1')(x)
+    x = layers.Dense(128, activation='relu',
+                     name='critic_dense2')(x)
+    x = layers.Dense(64, activation='relu',
+                     name='critic_dense3')(x)
+    x = layers.Dense(IQN_SUPPORT, activation='linear',dtype='float32',
+                           name='critic_dense4')(x)
+    outputs = tf.squeeze(x, name='critic_squeeze')
+    outputs = layers.Activation('linear',dtype=tf.float32,
+                                name='critic_float32')(outputs)
+
+    model = keras.Model(
+        inputs=[action_input,] + encoder_inputs, 
+        outputs=outputs,
+        name='critic'
+    )
+
+    return model
