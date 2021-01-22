05d7e532a47e8fc1c2a5f27b6ae61523b406f2e0
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-22 23:06:58 +0900

    clip -> add noise -> clip

--- Agent.py
+++ Agent.py
@@ -198,9 +198,15 @@ class Player():
         """
         processed_state = self.pre_processing(before_state)
         raw_action = self.models['actor'](processed_state, training=False)
-        action = self.oup_noise(raw_action)
+        clipped_raw_action = tf.clip_by_value(
+            raw_action,
+            self.action_space.low,
+            self.action_space.high,
+            name='clip_after_noise'
+        )
+        noised_action = self.oup_noise(clipped_raw_action)
         action = tf.clip_by_value(
-            action,
+            noised_action,
             self.action_space.low,
             self.action_space.high,
             name='clip_after_noise'
