f89947c9d9235a3417e8dd00259685ecf0b6a2b0
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-01-30 11:44:40 +0900

    not what I intended

--- Agent.py
+++ Agent.py
@@ -206,12 +206,20 @@ class Player():
             tf.summary.scalar('a0_raw', raw_action[0][0], self.total_steps)
             tf.summary.scalar('a1_raw', raw_action[0][1], self.total_steps)
         noised_action = self.oup_noise(raw_action)
-        if tf.random.uniform(())<hp.OUP_CLIP:
+        if tf.random.uniform(())<hp.OUP_clip:
+            raw_action = tf.clip_by_value(
+                raw_action,
+                self.action_space.low,
+                self.action_space.high,
+            )
+            noised_action = self.oup_noise(raw_action)
             noised_action = tf.clip_by_value(
                 noised_action,
                 self.action_space.low,
                 self.action_space.high,
             )
+        else:
+            noised_action = self.oup_noise(raw_action)
         return noised_action
 
     @tf.function
